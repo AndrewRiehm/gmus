450b2e1b1886cd4d7e9649418cb99cd7bb53ca09
Andrew Riehm <309609+AndrewRiehm@users.noreply.github.com>
2025-01-25 23:50:46 -0700

    feat(#4): added a mock repo and tests, with makefile shortcuts (#8)

--- cmd/gmus/main.go
+++ cmd/gmus/main.go
@@ -2,11 +2,26 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	musicrepo "github.com/AndrewRiehm/gmus/pkg/music_repository"
 )
 
 func main() {
 	var weirdAl = musicrepo.Artist("Weird Al")
-	fmt.Printf("hello, %v!\n", weirdAl)
+
+	fmt.Printf("Hello, %v!\n\n", weirdAl)
+
+	repo := musicrepo.NewMockMusicRepo()
+
+	albums, err := repo.GetAlbumsForArtist(weirdAl)
+	if err != nil {
+		fmt.Printf("error: %v\n", err)
+		os.Exit(-1)
+	}
+
+	fmt.Printf("%d albums for %v:\n", len(albums), weirdAl)
+	for _, album := range albums {
+		fmt.Printf("\t%q\n", album.Name)
+	}
 }
